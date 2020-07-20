48072e278d8ddf95750f1736d76c0334874abd75
Kimmo Sääskilahti <kimmo.saaskilahti@silo.ai>
2020-07-20 09:10:49 +0300

    Add replace_chunk:

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Hi! I'm Kimmo, a software developer at [Silo AI](https://silo.ai/).
 
 See me online resume at [ksaaskil.github.io](https://ksaaskil.github.io).
+
+<!-- updated_on starts -->
+<!-- updated_on ends -->
--- build_readme.py
+++ build_readme.py
@@ -1,8 +1,11 @@
 """
-Inspired by https://github.com/simonw/simonw/blob/main/build_readme.py
+Inspired by and some of the code copied from
+https://github.com/simonw/simonw/blob/main/build_readme.py
 """
+from datetime import datetime
 import pathlib
 import os
+import re
 
 from python_graphql_client import GraphqlClient
 
@@ -12,10 +15,26 @@ client = GraphqlClient(endpoint="https://api.github.com/graphql")
 
 TOKEN = os.environ.get("API_TOKEN", "")
 
+
+def replace_chunk(content, marker, chunk, inline=False):
+    r = re.compile(
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+        re.DOTALL,
+    )
+    if not inline:
+        chunk = "\n{}\n".format(chunk)
+    chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(marker, chunk, marker)
+    return r.sub(chunk, content)
+
+
 if __name__ == "__main__":
     readme = root / "README.md"
 
     readme_content = readme.open("r").read()
+
+    updated_at_md = "Updated at {date}".format(date=datetime.now())
+
+    rewritten = replace_chunk(readme_content, "updated_at", updated_at_md)
 
     new_readme_content = readme_content  # TODO
 
